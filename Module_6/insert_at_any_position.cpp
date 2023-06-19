5eea553e2a2f61d681f26d4390571bf2ccd6aeb7
shufol islam <shufolislam1255@gmail.com>
2023-06-19 22:23:43 +0600

    insert at aanyy pos

---
+++ Module_6/insert_at_any_position.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node *next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void inset_at_any_position(Node *head, int pos,  int val){
+    Node *newnode = new Node(val);
+}
+int main(){
+    Node *head = NULL;
+    return 0;
+}
