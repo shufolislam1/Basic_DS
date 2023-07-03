33dffd480780b89741d1db1ff8c1d12a80f78d8d
shufol islam <shufolislam1255@gmail.com>
2023-07-03 23:47:55 +0600

    in the mid

---
+++ module_10/operation_functions.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> myList = {30, 1, 10, 33, 90, 10, 43, 65, 78, 10};
+    // myList.remove(10);
+    // myList.sort(); //ascending order
+    // myList.sort(greater<int>()); //decending order
+    // myList.unique();
+    // myList.reverse();
+
+    // for (int val : myList)
+    // {
+    //     cout << val << endl;
+    // }
+
+    // acessing 1st element
+    // cout<<myList.front();
+
+    // acessing last element
+    // cout<<myList.back();
+
+    // acessing any element
+    // cout<<*next(myList.begin(), 6);
+    return 0;
+}
